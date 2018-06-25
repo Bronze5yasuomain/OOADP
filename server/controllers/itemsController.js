8f23b5813ec496aabc370a66b953c0d8653180cf
Divyeshwaren <173087M@mymail.nyp.edu.sg>
2018-06-25 11:46:01 +0800

    update on itemcontroller for seller id

--- server/controllers/itemsController.js
+++ server/controllers/itemsController.js
@@ -10,6 +10,7 @@ exports.insert = function (req,res) {
         description: req.body.description,
         condition: req.body.condition,
         category: req.body.category,
+        seller_id: req.body.seller_id,
     }
     ItemModel.create(itemdata).then((newRecord, created) => {
         if (!newRecord){
@@ -61,6 +62,7 @@ exports.update=function(req, res){
         description: req.body.description,
         condition: req.body.condition,
         category: req.body.category,
+        seller_id: req.body.seller_id,
     }
     ItemModel.update(updateData,{where:{id:record_num} }).then((updatedRecord)=>{
         if (!updatedRecord || updatedRecord == 0){
