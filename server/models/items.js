d31be5bb2f2934824c394b6ed0b3931dd94d8f20
Christy <173209W@mymail.nyp.edu.sg>
2018-07-06 11:12:25 +0800

    some changes

--- server/models/items.js
+++ server/models/items.js
@@ -48,8 +48,8 @@ ItemModel.sync({ force:false, logging:console.log }).then(() => {
         description:"5.5 inch Retina Display, A10 Fusion Chip",
         condition:"New",
         category:"Electronics",
-        quantity_left: "2",
-        seller_id:"2",
+        quantity_left: 2,
+        seller_id:1
 
     });
 //     console.log("Items synced");
