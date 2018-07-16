a0452a495ebbd91febb64354d8d9b86593dc89c5
ernest eu <twps.ernest@hotmail.com>
2018-07-16 15:15:25 +0800

    Added a whole bunch of controllersa

---
+++ server/controllers/itemViewIndividualController.js
@@ -0,0 +1,29 @@
+var ItemModel = require('../models/items');
+var myDatabase = require('./database');
+var sequelize = myDatabase.sequelize;
+//image shit modules
+var fs = require('fs');
+var mime = require('mine');
+var gravatar = require('gravatar');
+//set image file types
+var IMAGE_TYPES = ['image/jpeg', 'image/jpg', 'image/png'];
+
+var Images = require('../models/images')
+
+
+
+exports.list=function(req, res){
+    ItemModel.findAll({
+        attributes: ['id','name','price','description','condition','category']
+    }).then(function (items) {
+        res.render('index', {
+            title:"Item List",
+            itemList: items,
+            urlPath: req.protocol + "://" + req.get("host") + req.url
+        });
+    }).catch((err) =>{
+        return res.status(400).send({
+            message: err
+        });
+    });
+};
