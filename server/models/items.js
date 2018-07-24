92ca9abd1fdebe209b03c57a805a143106db7856
ernest eu <twps.ernest@hotmail.com>
2018-07-25 01:30:15 +0800

    sync force false and refences changes

--- server/models/items.js
+++ server/models/items.js
@@ -8,11 +8,7 @@ const ItemModel = sequelize.define('Items',{
         type: Sequelize.INTEGER,
         primaryKey: true,
         autoIncrement:true,
-        references: {
-            model: 'Orders',
-            key: 'Order_id'
-        }
-
+        
     },
     name: {
         type: Sequelize.STRING
